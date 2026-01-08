4356c19d4435f59950e91099a1633b15dc5a2320
Gutts-n <57202549+Gutts-n@users.noreply.github.com>
2026-01-08 19:48:13 -0300

    Fixed jinja render error

--- ckanext/forums/tests/logic/action/test_issue.py
+++ ckanext/forums/tests/logic/action/test_issue.py
@@ -70,7 +70,7 @@ class TestIssueNewWithEmailing(object):
 
         # mock the render as it is easier to look at the variables passed in
         # than the rendered text
-        with mock.patch('ckanext.forums.logic.action.action.render_jinja2') \
+        with mock.patch('ckan.plugins.toolkit.render') \
                 as render_mock:
             issue_create_result = toolkit.get_action('forum_create')(
                 context={'user': creator['name']},
@@ -233,7 +233,7 @@ class TestIssueComment(object):
 
         # mock the render as it is easier to look at the variables passed in
         # than the rendered text
-        with mock.patch('ckanext.forums.logic.action.action.render_jinja2') \
+        with mock.patch('ckan.plugins.toolkit.render') \
                 as render_mock:
             helpers.call_action(
                 'forum_comment_create',
